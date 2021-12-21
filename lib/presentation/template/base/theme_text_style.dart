ce557d0cdd3695c42c2aa4e8c859c0cfe6eabb15
onyxclubpride <onyxclubpride@gmail.com>
2021-12-21 22:53:26 +0900

    font size created

--- lib/presentation/template/base/theme_text_style.dart
+++ lib/presentation/template/base/theme_text_style.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:smart_house_flutter/presentation/template/base/template.dart';
-   const fontFamilyLato = "Lato";
+  
 
- const notoR10 = TextStyle(
+const fontFamilyLato = "Lato";
+const fontFamilyFutura = "Futura"
+
+
+
+const futuraR14 = TextStyle(
   fontWeight: FontWeight.w500,
-  fontFamily: fontFamilyLato,
-  fontStyle: FontStyle.normal,
-  fontSize: 10.0,
-);
+  fontFamily: fontFamilyFutura,
+  fontSize: 14.0,
+  // fontStyle:FontStyle.normal
+)
+
+const futuraM16 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamilyFutura,
+  fontSize: 16.0,
+  // fontStyle:FontStyle.normal
+)
+
+const futuraM20 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamilyFutura,
+  fontSize: 20.0,
+  // fontStyle:FontStyle.normal
+)
+
+const futuraB45 = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontFamily: fontFamilyFutura,
+  fontSize: 45.0,
+  // fontStyle:FontStyle.normal
+)
+
+
+
+
+const latoR14 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamilyFuture,
+  fontSize: 14.0,
+  // fontStyle:FontStyle.normal
+)
+
+const latoM16 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamilyFuture,
+  fontSize: 16.0,
+  // fontStyle:FontStyle.normal
+)
+
+const latoM20 = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontFamily: fontFamilyFuture,
+  fontSize: 20.0,
+  // fontStyle:FontStyle.normal
+)
+
+const latoB45 = TextStyle(
+  fontWeight: FontWeight.w900,
+  fontFamily: fontFamilyFuture,
+  fontSize: 45.0,
+  // fontStyle:FontStyle.normal
+)
