b2c8896bf384429522b9999446f52ebcf2107658
onyx nishat <onyxclubpride@gmail.com>
2022-01-09 15:37:38 +0900

    edit profile ui

--- lib/presentation/pages/edit_profile_page.dart
+++ lib/presentation/pages/edit_profile_page.dart
@@ -38,58 +38,131 @@ class _EditProfilePageState extends State<EditProfilePage> {
             withActionButton: false,
             titleIcon: _buildTitleIcon(),
             titleText: SizedText(text: 'Edit Profile', textStyle: latoM20),
-            child: Form(
-              key: _formKeyEditProfilePage,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // if (descriptionController.text.isNotEmpty)
-                    DefaultBanner(
-                      bgColor: ThemeColors.black,
-                      child: Container(
-                        padding: EdgeInsets.all(12.w),
-                        child: SpacedColumn(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedText(
-                                text: 'Post details',
-                                textStyle: latoR14.copyWith(
-                                    color: ThemeColors.fontWhite)),
-                            Container(
-                              margin: EdgeInsets.symmetric(vertical: 6.h),
-                              child: Divider(
-                                  thickness: 1.w,
-                                  color: ThemeColors.borderDark),
-                            ),
-                            SpacedColumn(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  if (state.apiState.postDetail.description !=
-                                      null)
-                                    InputLabel(label: 'Description'),
-                                  if (state.apiState.postDetail.description !=
-                                      null)
-                                    PostCreateInput(
-                                      maxlines: 10,
-                                      validator: Validator.validateText,
-                                      controller: descriptionController,
+            bottomPadding: 15.h,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height - 140.h,
+              child: Form(
+                key: _formKeyEditProfilePage,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // if (descriptionController.text.isNotEmpty)
+                      DefaultBanner(
+                        bgColor: ThemeColors.black,
+                        child: Container(
+                          padding: EdgeInsets.all(12.w),
+                          child: SpacedColumn(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedText(
+                                  text: 'User',
+                                  textStyle: latoR14.copyWith(
+                                      color: ThemeColors.fontWhite)),
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 6.h),
+                                child: Divider(
+                                    thickness: 1.w,
+                                    color: ThemeColors.borderDark),
+                              ),
+                              SpacedColumn(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      InputLabel(label: 'User Name'),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      PostCreateInput(
+                                        maxlines: 1,
+                                        validator: Validator.validateText,
+                                        controller: descriptionController,
+                                      ),
+                                    SizedBox(
+                                      height: 25.h,
                                     ),
-                                  if (state.apiState.postDetail.imageUrl !=
-                                      null)
-                                    DefaultBanner(
-                                      onTap: _onChooseImage,
-                                      height: 200.h,
-                                      child: FittedBox(
-                                          child: _getImageOrNotWidget(
-                                              state.apiState)),
-                                    ),
-                                ]),
-                          ],
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      InputLabel(label: 'Kakao Id'),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      PostCreateInput(
+                                        maxlines: 1,
+                                        validator: Validator.validateText,
+                                        controller: descriptionController,
+                                      ),
+                                  ]),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 25.h,
+                      ),
+                      DefaultBanner(
+                        bgColor: ThemeColors.black,
+                        child: Container(
+                          padding: EdgeInsets.all(12.w),
+                          child: SpacedColumn(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedText(
+                                  text: 'Password',
+                                  textStyle: latoR14.copyWith(
+                                      color: ThemeColors.fontWhite)),
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 6.h),
+                                child: Divider(
+                                    thickness: 1.w,
+                                    color: ThemeColors.borderDark),
+                              ),
+                              SpacedColumn(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      InputLabel(label: 'Current Password'),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      PostCreateInput(
+                                        maxlines: 1,
+                                        validator: Validator.validateText,
+                                        controller: descriptionController,
+                                      ),
+                                    SizedBox(
+                                      height: 25.h,
+                                    ),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      InputLabel(label: 'New Password'),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      PostCreateInput(
+                                        maxlines: 1,
+                                        validator: Validator.validateText,
+                                        controller: descriptionController,
+                                      ),
+                                    SizedBox(
+                                      height: 25.h,
+                                    ),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      InputLabel(label: 'Confirm Password'),
+                                    if (state.apiState.postDetail.description !=
+                                        null)
+                                      PostCreateInput(
+                                        maxlines: 1,
+                                        validator: Validator.validateText,
+                                        controller: descriptionController,
+                                      ),
+                                  ]),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
